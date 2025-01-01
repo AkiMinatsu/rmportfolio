e34668672e9799c5ac06c63bbcd38f047005af70
AkiMinatsu <haroldbas000@gmail.com>
2025-01-01 12:33:06 +0800

    smooth transaction

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  scrollToSection(sectionId: string): void {
+    const targetElement = document.getElementById(sectionId);
+    if (targetElement) {
+      const headerOffset = document.querySelector('header')?.clientHeight || 0;
+      const elementPosition = targetElement.offsetTop;
+      const offsetPosition = elementPosition - headerOffset;
 
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    }
+  }
 }
