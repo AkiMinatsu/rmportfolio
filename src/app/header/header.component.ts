7288a46b6a86d35813d3aaeaea66e2dadd469c5a
AkiMinatsu <haroldbas000@gmail.com>
2025-01-01 12:49:29 +0800

    inulit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,20 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  scrollToSection(sectionId: string): void {
-    const targetElement = document.getElementById(sectionId);
-    if (targetElement) {
-      const headerOffset = document.querySelector('header')?.clientHeight || 0;
-      const elementPosition = targetElement.offsetTop;
-      const offsetPosition = elementPosition - headerOffset;
 
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: 'smooth',
-      });
-    }
-  }
 }
